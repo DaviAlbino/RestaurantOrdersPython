388e6e9b13b49c0a4ab81058a516c504a7800e18
Davi Albino <negroalbino_@hotmail.com>
2023-05-22 23:19:25 -0300

    test: ingredients

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,15 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
-# Req 1 começando o projeto
+# Req 1
 def test_ingredient():
-    pass
+    rest_meat = Restriction.ANIMAL_MEAT
+    rest_derived = Restriction.ANIMAL_DERIVED
+    repr_response = "Ingredient('queijo mussarela')"
+    hash_response = hash("queijo mussarela")
+    egg = Ingredient("ovo")
+    assert Ingredient("queijo mussarela").name == "queijo mussarela"
+    assert Ingredient("queijo mussarela").__repr__() == repr_response
+    assert Ingredient("queijo mussarela").__hash__() == hash_response
+    assert Ingredient("ovo").__eq__(egg) is True
+    assert Ingredient("carne").restrictions == {rest_meat, rest_derived}
