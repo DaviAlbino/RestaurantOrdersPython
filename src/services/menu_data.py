c7070b7bc789df57fbde6874d1e91af9dafd91a9
Davi Albino <negroalbino_@hotmail.com>
2023-05-23 01:29:34 -0300

    feat: menu data

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,19 @@
+import pandas as pd
+from models.dish import Ingredient
+from models.dish import Dish
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.menu = dict()
+        self.dishes = set()
+        csv_path = pd.read_csv(source_path).itertuples(index=False)
+
+        for dish, price, ingredient, amount in csv_path:
+            if dish not in self.menu.keys():
+                self.menu[dish] = Dish(dish, price)
+                self.dishes.add(self.menu[dish])
+
+            new_ingredient = Ingredient(ingredient)
+            self.menu[dish].add_ingredient_dependency(new_ingredient, amount)
